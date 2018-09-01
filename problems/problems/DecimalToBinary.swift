16b5bda8fdd38ccd79309262da9632c0c8713781
Aariz Rahman <aariz.ice@gmail.com>
2018-09-01 12:08:59 -0700

    Created Problems project and Added DecimalToBinary

---
+++ problems/problems/DecimalToBinary.swift
@@ -0,0 +1,14 @@
+//
+//  DecimalToBinary.swift
+//  problems
+//
+//  Created by Azizur Rahman on 2018-09-01.
+//  Copyright © 2018 BigDo. All rights reserved.
+//
+
+import Foundation
+
+
+func getBinaryDigitis ()-> String {
+    return "Hello"
+}
---
+++ problems/problems/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  problems
+//
+//  Created by Azizur Rahman on 2018-09-01.
+//  Copyright © 2018 BigDo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+print(getBinaryDigitis())
+
